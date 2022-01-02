d8042417d1162b38fef941af9b881d2506e3221a
Rakesh Chaudhary <93024101+Chaudhary1rakesh@users.noreply.github.com>
2022-01-02 12:45:59 +0530

    Add files via upload

---
+++ GCD_recur.java
@@ -0,0 +1,19 @@
+public class GCD_recur {
+   
+        public static int gcd(int n1,int n2)
+        {
+            if(n2!=0)
+            {
+                return gcd(n2,n1%n2);
+            }
+            else
+            return n1;
+        }
+        public static void main(String[] args) {
+        int n1=4,n2=8;
+        int gcd=gcd(n1,n2);
+        System.out.print("GCD is:"+gcd);
+        
+        }
+    }    
+
---
+++ power_recur.java
@@ -0,0 +1,23 @@
+// import java.lang.Math;
+ class power_recur {
+     static int result;
+    public static int Power(int n,int p)
+    {
+        if(p==0)
+        {
+            return 1;
+        }
+         result=n*Power(n,p-1);
+        return result;
+       
+
+    }
+    public static void main(String[] args) {
+      int n=2;
+      int p=5;
+      Power(n,p);
+      System.out.println("Power of Given numbers:"+n+"^"+p+"="+result);
+
+    }
+    
+}
